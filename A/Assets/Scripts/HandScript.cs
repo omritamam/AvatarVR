1e6138aeba3e00ca0b64ca98cd1eb309a2d5da29
GalShaffir <galshaffir@mail.huji.ac.il>
2021-06-20 15:31:49 +0300

    20/6/21

--- A/Assets/Scripts/HandScript.cs
+++ A/Assets/Scripts/HandScript.cs
@@ -21,97 +21,114 @@ public class HandScript : MonoBehaviour
     [SerializeField] private GameObject textObject1;
     [SerializeField] private GameObject textObeject2;
     [SerializeField] private GameObject projectile;
+    [SerializeField] private GameObject head;
     private Vector3[] up = new Vector3[10];
-    
-    
-    
-    
+
+
+
+
     public InputMaster myControls;
 
-     void Awake()
+    void Awake()
     {
-        textObeject.GetComponent<TextMesh>().text ="started";;
+        textObeject.GetComponent<TextMesh>().text = "started";
+        ;
         for (int i = 0; i < 10; i++)
         {
             up[i] = new Vector3(0, i * 0.1f + 0.1f, 0);
         }
+
         myControls = new InputMaster();
         Debug.Log(up);
-   
+
         myControls.player.record.performed += _ => record();
     }
 
-     void record()
-     { 
-         StartCoroutine(recordCoroutine());
+    void record()
+    {
+        StartCoroutine(recordCoroutine());
     }
 
-     private IEnumerator recordCoroutine()
-     {
-         Vector3 forward = transform.forward;
-         forward.y = 0;
-         forward = forward.normalized;
-         GameObject rock = Instantiate(projectile, transform.position + (0.1f * forward),
-             quaternion.identity, rightHandObject.transform);
-         
-         
-         
-         Queue<Vector3> movement = new Queue<Vector3>();
-         Vector3 prime = new Vector3(0,0,0);
-         while(myControls.player.record.ReadValue<float>()>0)
-         {
-             if (movement.Count == 0)
-             {
-                 prime = rightHandObject.transform.position;
-             }
-             
-             Vector3 cur = rightHandObject.transform.position - prime;
+    private IEnumerator recordCoroutine()
+    {
+        Vector3 forward = transform.forward;
+        forward.y = 0;
+        forward = forward.normalized;
+        GameObject rock = Instantiate(projectile, transform.position + (0.1f * forward),
+            quaternion.identity, rightHandObject.transform);
+        
+        Queue<Vector3> movement = new Queue<Vector3>();
+        Vector3 prime = new Vector3(0, 0, 0);
+        while (myControls.player.record.ReadValue<float>() > 0)
+        {
+            if (movement.Count == 0)
+            {
+                prime = rightHandObject.transform.position;
+            }
 
-             textObeject.GetComponent<TextMesh>().text = cur.ToString();
-             if (movement.Count == 10)
-             {
-                 movement.Dequeue();
-                 textObeject.GetComponent<TextMesh>().text ="10";;
-             }
-             movement.Enqueue(cur);
-             yield return null;
-         }
-         
-         textObject1.GetComponent<TextMesh>().text ="clearing";
-         if (checkMove(movement))
-         {
-             rock.transform.parent = null;
-             
-         }
-         Destroy(rock);
-         movement.Clear();
-     }
+            Vector3 cur = rightHandObject.transform.position - prime;
 
-     private bool checkMove(Queue<Vector3> movement)
-     {
-         if (movement.Count != 10)
-         {
-             textObeject2.GetComponent<TextMesh>().text ="movement too short in time " + movement.Count.ToString();
-             return false;
-         }
-         textObeject2.GetComponent<TextMesh>().text ="movement magnitude is " + movement.Peek().magnitude.ToString();
-         if (movement.Peek().magnitude < 1)
-         {
-             return false;
-         }
-         textObeject.GetComponent<TextMesh>().text =Vector3.Dot(up[9], movement.Peek().normalized).ToString();
+            textObeject.GetComponent<TextMesh>().text = cur.ToString();
+            if (movement.Count == 10)
+            {
+                movement.Dequeue();
+                textObeject.GetComponent<TextMesh>().text = "10";
+                ;
+            }
 
-         if (Vector3.Dot(up[9], movement.Peek().normalized) < 0.8)
-         {
-             
-             return false;
-         }
-         textObject1.GetComponent<TextMesh>().text ="made it";
-         return true;
-     }
-     
+            movement.Enqueue(cur);
+            yield return null;
+        }
 
-     void OnEnable()
+        textObject1.GetComponent<TextMesh>().text = "clearing";
+        if (checkMove(movement))
+        {
+            rock.transform.parent = head.transform;
+            rock.transform.position = head.transform.position + (0.5f * forward);
+
+        }
+        else
+        {
+            Destroy(rock);
+            movement.Clear();
+        }
+        
+    }
+
+    private bool checkMove(Queue<Vector3> movement)
+    {
+        if (movement.Count != 10)
+        {
+            textObeject2.GetComponent<TextMesh>().text = "movement too short in time " + movement.Count.ToString();
+            return false;
+        }
+
+        textObeject2.GetComponent<TextMesh>().text = "movement magnitude is " + movement.Peek().magnitude.ToString();
+        if (movement.Peek().magnitude < 1)
+        {
+            return false;
+        }
+
+        textObeject.GetComponent<TextMesh>().text = Vector3.Dot(up[9], movement.Peek().normalized).ToString();
+
+        if (Vector3.Dot(up[9], movement.Peek().normalized) < 0.8)
+        {
+
+            return false;
+        }
+
+        textObject1.GetComponent<TextMesh>().text = "made it";
+        return true;
+    }
+
+    private void goodMove()
+    {
+        
+
+    }
+
+
+    void OnEnable()
     {
         myControls.Enable();
     }
